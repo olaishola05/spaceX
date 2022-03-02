4b4985498a896019c522b574f7e2e2800b5268a8
Ola Ishola <45001916+netman5@users.noreply.github.com>
2022-03-02 20:54:27 +0100

    Remove commented code

--- src/Components/Missions.js
+++ src/Components/Missions.js
@@ -5,13 +5,6 @@ import { fetchMissions } from '../redux/missions/missions';
 
 function Missons() {
   const dispatch = useDispatch();
-  // const missions = [
-  //   {
-  //     mission: 'spacex',
-  //     description:
-  //       'In this activity, your team will prepare a Kanban board with a GitHub project. We prepared a template that you need to copy. All tasks in the template board have assigned workload and a category. Your job will be to split the tasks in a way that gives each member a chance to work with the tasks from all categories and so that the estimated time is divided fairly between all members. In order to copy the Kanban board quickly, you will use a special script prepared for you.',
-  //   },
-  // ];
 
   useEffect(() => {
     dispatch(fetchMissions());
